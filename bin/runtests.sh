d7d5fec24130befb4f08aa5aea1ab27b0c7eb2ed
Tom Stivers <tstivers@egov.com>
2012-07-14 00:19:26 -0500

    only run tests on official maps (ignore tests folder)

--- bin/runtests.sh
+++ bin/runtests.sh
@@ -1,3 +1,3 @@
-find ../maps -name *.map -exec mono LambdaLifter.Cli.exe {} \; > log.txt
+find ../maps/*.map -exec mono LambdaLifter.Cli.exe {} \; > log.txt
 grep MapState log.txt
 echo Won: `grep Won log.txt | wc -l`  Lost: `grep MapState log.txt | grep -v Won | wc -l`  Score: `grep Score log.txt | awk '{ sum += $2 } END { print sum }'`
