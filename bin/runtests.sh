e11cad5f2ac7415b778daa775169cabd3740ac4c
Tom Stivers <tstivers@egov.com>
2012-07-13 19:46:08 -0500

    Updating runtests again

--- bin/runtests.sh
+++ bin/runtests.sh
@@ -1,1 +1,4 @@
-find ../maps -name *.map -exec mono client.exe \< {} \;
+find ../maps -name *.map -exec echo {} \; -exec mono client.exe {} \; > log.txt
+grep MapState log.txt
+echo Won: `grep Won log.txt | wc -l`
+echo Lost: `grep MapState log.txt | grep -v Won | wc -l`
