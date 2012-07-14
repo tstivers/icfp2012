28fd773e1b57a406d6ffec7bb8c3aaf44dc34aaa
Tom Stivers <tstivers@egov.com>
2012-07-14 12:19:42 -0500

    adding trampoline maps

--- src/LambdaLifter.Visualizer/Program.cs
+++ src/LambdaLifter.Visualizer/Program.cs
@@ -15,7 +15,7 @@ namespace LambdaLifter.Visualizer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
